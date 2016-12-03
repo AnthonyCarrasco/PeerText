2d84baaed7033ab427c91bd5d681b09d3ec3bb4f
Angel Cortez <acort3255@gmail.com>
2016-12-03 17:59:39 -0500

    Added async login

--- src/app/Login/Model/LoginAPIDataManager.java
+++ src/app/Login/Model/LoginAPIDataManager.java
@@ -2,29 +2,67 @@ package app.Login.Model;
 
 import app.Login.*;
 import com.google.gson.*;
-import org.asynchttpclient.*;
+import java.lang.reflect.*;
+import com.google.gson.reflect.*;
+import org.asynchttpclient.AsyncCompletionHandler;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.BoundRequestBuilder;
+import org.asynchttpclient.Response;
+import static org.asynchttpclient.Dsl.asyncHttpClient;
+import java.util.*;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Created by Angel on 11/26/16.
  */
 public class LoginAPIDataManager {
-    public Boolean attemptLogin(LoginItem loginItem)
-    {
+    public Boolean attemptLogin(LoginItem loginItem) {
 
-        Gson test = new Gson();
-        String value = test.toJson(loginItem);
+        Gson jsonSeralizer = new Gson();
+        String loginJSON = jsonSeralizer.toJson(loginItem);
 
+        try (AsyncHttpClient asyncHttpClient = asyncHttpClient()) {
+            Future<Boolean> success = asyncHttpClient
+                    .preparePost("http://lowCost-env.kydrpmgvhm.us-east-1.elasticbeanstalk.com/login.php")
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("Accept", "application/json")
+                    .setBody(loginJSON)
+                    .execute(
+                            new AsyncCompletionHandler<Boolean>() {
+                                @Override
+                                public Boolean onCompleted(Response response) throws Exception {
+                                    // Was the login successful
+                                    Type type = new TypeToken<Map<String, String>>() {
+                                    }.getType();
+                                    Map<String, String> jsonMap = jsonSeralizer.fromJson(response.getResponseBody(), type);
+                                    System.out.println("JSONMAP "+jsonMap);
+                                    if (!jsonMap.get("user_id").isEmpty()) {
+                                        return true;
+                                    } else {
+                                        System.out.println(jsonMap.get("error_message"));
+                                        return false;
+                                    }
+                                }
+                            }
+                    )
+                    .toCompletableFuture();
+            return success.get();
 
-        if (loginItem.username.toLowerCase().equals("angel") && loginItem.password.equals("password"))
-        {
-            System.out.println(value);
-            return true;
-        }
-
-        else
-        {
-            //System.out.println("Failed to login!!!!");
+        } catch (InterruptedException e) {
+            //e.printStackTrace();
+            return false;
+        } catch (ExecutionException e) {
+            //e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            //e.printStackTrace();
             return false;
         }
+
     }
+
 }
