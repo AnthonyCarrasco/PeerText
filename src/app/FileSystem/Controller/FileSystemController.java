09e1567d297638ac76bbaf9a52e3d8313f7c9c79
Angel Cortez <acort3255@gmail.com>
2016-12-04 17:52:10 -0500

    FileSystem downloads files

--- src/app/FileSystem/Controller/FileSystemController.java
+++ src/app/FileSystem/Controller/FileSystemController.java
@@ -2,11 +2,14 @@ package app.FileSystem.Controller;
 
 import app.FileSystem.FileItem;
 import app.FileSystem.Model.FileSystemModel;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.collections.*;
 import javafx.scene.control.TableView;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,7 +18,13 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by Angel on 11/23/16.
@@ -27,17 +36,46 @@ public class FileSystemController extends Parent{
     @FXML private Button btnEditAccess;
     @FXML private Button btnEditViewAccess;
     @FXML private TableView<FileItem> fileTable;
+    @FXML private TableColumn<FileItem, String> fileNameCol;
+    ObservableList<FileItem> files;
 
     public FileSystemModel model;
 
     public void loadFiles(String user_id)
     {
+
+        System.out.println("Table: " + fileTable);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                fileNameCol.setCellValueFactory(
+                        new PropertyValueFactory<FileItem, String>("title"));
+            }
+        });
+
         this.user_id = user_id;
         model.getFiles(user_id);
     }
 
     public void returnFiles(FileItem[] data)
     {
-        fileTable.
+        files = FXCollections.observableArrayList(data);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+
+                fileTable.setItems(files);
+
+            }
+        });
     }
+
+
+    @FXML protected  void handleCreateButtonAction(ActionEvent event)
+    {
+        System.out.println("Create file pressed");
+    }
+
 }
--- src/app/FileSystem/FileItem.java
+++ src/app/FileSystem/FileItem.java
@@ -7,4 +7,16 @@ public class FileItem {
     public String textfile_id;
     public String owner_id;
     public String title;
+
+    public FileItem(String textfile_id, String owner_id, String title)
+    {
+        this.textfile_id = textfile_id;
+        this.owner_id = owner_id;
+        this.title = title;
+    }
+
+    public String getTitle()
+    {
+        return  this.title;
+    }
 }
